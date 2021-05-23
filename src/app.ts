24e21bdbc0674138e9c86713b8c6d55c4a4a7c60
Max <elantiswing@hotmail.com>
2021-05-23 12:34:28 +0200

    set env variables

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, { Application, Request, Response } from 'express'
+import { config } from 'dotenv'
+
+// Start dotenv config
+config()
 
 const app: Application = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/test', (req: Request, res: Response) => {
   res.send('Hello world')
