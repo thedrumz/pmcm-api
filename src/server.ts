72d931796b9691043d086bc669e1c251204d45db
Max <elantiswing@hotmail.com>
2021-07-23 21:34:46 +0200

    dockerize project

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { config } from 'dotenv'
 // Start dotenv config
 config()
 
-const port = process.env.PORT || 3000
+const port = process.env.NODE_LOCAL_PORT || 3000
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port} !`)
